46c5460c7d6494dd4f474b6a5635df67fb24c37c
olowo-elisha-ibukun <elishaibukunolowo@gmail.com>
2025-11-12 22:27:27 +0100

    olowo

--- week01/csharp-1/obj/Debug/net9.0/csharp-1.AssemblyInfo.cs
+++ week01/csharp-1/obj/Debug/net9.0/csharp-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c285d2fd8b6ef5b68b82b4ca27c60ab028ab11b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db24e2861f568dc6af6cc775f0d611cfde9580c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
